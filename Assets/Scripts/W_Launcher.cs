8c3ffc658f772c9f46babcd91a4d20088f4fbfc7
Joseph Dasilva <jojods1125@gmail.com>
2021-02-24 15:31:35 -0500

    Sprayer fully functional, code organization
    The sprayer is fully functional. It shoots raycasts up to a specified distance, paving the way for short-ranged high-damage attacks. More organizational changes were made to weapon scripts to make them easily readable.

--- Assets/Scripts/W_Launcher.cs
+++ Assets/Scripts/W_Launcher.cs
@@ -4,19 +4,26 @@
 public class W_Launcher : W_Shootable
 {
     [Header("Launcher Properties")]
+
     [Min(0)]
     [Tooltip("Speed at which the projectile travels")]
     public float projectilePower;
+
     [Tooltip("Prefab for the projectile")]
     public GameObject projectilePrefab;
+
     [Tooltip("Determines if gravity affects projectile; set to true for no gravity")]
     public bool rocketPowered;
+
     [Space(7)]
+
     [Min(0)]
     [Tooltip("Radius of AoE explosion; set to 0 for no explosion")]
     public float explosionRadius;
+
     [Tooltip("Damage dealt at the core of the projectile's explosion or impact point")]
     public float coreDamage;
+
     [Tooltip("Pushback dealt at the core of the projectile's explosion or impact point")]
     public float corePushback;
     
--- Assets/Scripts/W_Shootable.cs
+++ Assets/Scripts/W_Shootable.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class W_Shootable : Weapon
 {
     [Header("Firing Properties")]
+
     [Min(0)]
     [Tooltip("Delay between shots in seconds")]
     public float fireRate;
+
     [Min(0)]
     [Tooltip("Capacity of ammo for weapon")]
     public int ammoCapacity;
+
     [Tooltip("Pushback force per bullet fired")]
     public float pushback;
 }
