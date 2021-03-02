51666ff06f0620b24a23bc95b2a9225b207a5385
Joseph Dasilva <jojods1125@gmail.com>
2021-03-02 01:46:53 -0500

    Smash Bros Find Mii stage, launcher projectiles ignore bounds
    The Find Mii stage from Super Smash Bros Ultimate has been recreated in the J_Smash scene. Additionally, launcher projectiles ignore objects with the "Bound" layer.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -34,60 +34,63 @@ public class Projectile : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // Creates explosion if there is an explosion radius
-        if (explosionRadius > 0)
+        if (other.gameObject.layer != LayerMask.NameToLayer("Bound"))
         {
-            // Spawn explosion prefab
-            GameObject projectile = Instantiate(prefab_Explosion, transform.position, Quaternion.identity);
-            projectile.transform.localScale = new Vector3(explosionRadius * 2, explosionRadius * 2, explosionRadius * 2);
-            Destroy(projectile, 0.5f);
-
-            // Bit shift the index of the layer (8) to get a bit mask for solid ground.
-            int layerMask = 1 << 8;
-
-            // This would cast rays only against colliders in layer 8.
-            // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-            layerMask = ~layerMask;
-
-            // Checks if explosion hits any objects not in the solid ground layer
-            Collider[] exploded = Physics.OverlapSphere(gameObject.transform.position, explosionRadius, layerMask);
-
-            // For each object hit
-            for (int i = 0; i < exploded.Length; i++)
+            // Creates explosion if there is an explosion radius
+            if (explosionRadius > 0)
             {
-                /// TODO: Make players in explosion take damage
-                if (exploded[i].gameObject.layer == LayerMask.NameToLayer("Player"))
+                // Spawn explosion prefab
+                GameObject projectile = Instantiate(prefab_Explosion, transform.position, Quaternion.identity);
+                projectile.transform.localScale = new Vector3(explosionRadius * 2, explosionRadius * 2, explosionRadius * 2);
+                Destroy(projectile, 0.5f);
+
+                // Bit shift the index of the layer (8) to get a bit mask for solid ground.
+                int layerMask = 1 << 8;
+
+                // This would cast rays only against colliders in layer 8.
+                // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+                layerMask = ~layerMask;
+
+                // Checks if explosion hits any objects not in the solid ground layer
+                Collider[] exploded = Physics.OverlapSphere(gameObject.transform.position, explosionRadius, layerMask);
+
+                // For each object hit
+                for (int i = 0; i < exploded.Length; i++)
                 {
-                    float damageMultiplier = 1 / (explosionRadius / (explosionRadius - Vector3.Distance(exploded[i].ClosestPoint(gameObject.transform.position), gameObject.transform.position)));
-                    damageMultiplier = Mathf.Min(Mathf.Max(damageMultiplier, 0), 1);
-                    exploded[i].gameObject.GetComponent<Player>().DecreaseHealth(coreDamage * damageMultiplier);
+                    /// TODO: Make players in explosion take damage
+                    if (exploded[i].gameObject.layer == LayerMask.NameToLayer("Player"))
+                    {
+                        float damageMultiplier = 1 / (explosionRadius / (explosionRadius - Vector3.Distance(exploded[i].ClosestPoint(gameObject.transform.position), gameObject.transform.position)));
+                        damageMultiplier = Mathf.Min(Mathf.Max(damageMultiplier, 0), 1);
+                        exploded[i].gameObject.GetComponent<Player>().DecreaseHealth(coreDamage * damageMultiplier);
+                    }
+
+                    // Add explosion force to rigidbody if exists
+                    Rigidbody rb = exploded[i].GetComponent<Rigidbody>();
+                    if (rb != null) { rb.AddExplosionForce(corePushback, gameObject.transform.position, explosionRadius); }
                 }
 
-                // Add explosion force to rigidbody if exists
-                Rigidbody rb = exploded[i].GetComponent<Rigidbody>();
-                if (rb != null) { rb.AddExplosionForce(corePushback, gameObject.transform.position, explosionRadius); }
+                Destroy(gameObject);
             }
-
-            Destroy(gameObject);
-        }
-        // Does not create an explosion if there is no explosion radius
-        else
-        {
-            // Inflict pushback on collided object if it has a RigidBody
-            Rigidbody rb = other.GetComponent<Rigidbody>();
-            if (rb != null)
+            // Does not create an explosion if there is no explosion radius
+            else
             {
-                Vector3 direction = other.gameObject.transform.position - gameObject.transform.position;
-                rb.AddForce(direction.normalized * corePushback);
-            }
+                // Inflict pushback on collided object if it has a RigidBody
+                Rigidbody rb = other.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    Vector3 direction = other.gameObject.transform.position - gameObject.transform.position;
+                    rb.AddForce(direction.normalized * corePushback);
+                }
 
-            /// TODO: Make player take damage
-            if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
-            {
-                other.gameObject.GetComponent<Player>().DecreaseHealth(coreDamage);
-            }
+                /// TODO: Make player take damage
+                if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+                {
+                    other.gameObject.GetComponent<Player>().DecreaseHealth(coreDamage);
+                }
 
-            Destroy(gameObject);
+                Destroy(gameObject);
+            }
         }
     }
 
