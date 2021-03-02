3417734d02b3bb35d1ada3d3dd41667146cba268
Logan Hobbs <loganhbz@live.com>
2021-03-02 17:33:06 -0500

    Working Updates
    Updated branch to show work in other branches

--- Assets/Scripts/AchievementManager.cs
+++ Assets/Scripts/AchievementManager.cs
@@ -11,51 +11,30 @@ using UnityEngine;
 /// </summary>
 public class AchievementManager : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
+    // ===========================================================
+    //                    SINGLETON PATTERN
+    // ===========================================================
+    private static AchievementManager instance;
+    public static AchievementManager Instance()
     {
-        // ?
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        // ?
-    }
-
-    private static AchievementManager _instance;
-    public static AchievementManager Instance
-    {
-        get
-        {
-            if ( _instance == null )
-            {
-                _instance = FindObjectOfType<AchievementManager>();
-                if ( _instance == null )
-                {
-                    GameObject go = new GameObject();
-                    go.name = "SingletonController";
-                    _instance = go.AddComponent<AchievementManager>();
-
-                    DontDestroyOnLoad(go);
-                }
-            }
-
-            return _instance;
-        }
+        return instance;
     }
 
     private void Awake()
     {
-        if (_instance != null)
+        if ( instance != null && instance != this )
         {
-            Destroy(gameObject);
-            Debug.LogError("Destroyed a duplicate instance of AchievementManager!");
+            Destroy( this.gameObject );
         }
-        _instance = this;
-        DontDestroyOnLoad(this.gameObject);
+        else
+        {
+            instance = this;
+        }
     }
+
+    // ===========================================================
+    //                      EVENT SYSTEM
+    // ===========================================================
 
     public List<Achievement> achievements;
 
