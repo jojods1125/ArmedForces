89f25f209fc266e91aab261f4153047ce9e1e64a
Logan Hobbs <loganhbz@live.com>
2021-03-14 01:28:22 -0400

    Basic Menu
    Simple layout of menu system [Main, Loadout, Map]. Menu has buttons to go from one to next or back to the main menu. They are still just frames

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+	/// Change to the Map Selection scene
+	/// </summary>
+    public void MapSelection()
+	{
+        SceneManager.LoadScene( "L_MapSelectionMenu" );
+	}
+
+    /// <summary>
+	/// Change to the Weapon Loadout scene
+	/// </summary>
+    public void Loadout()
+	{
+        SceneManager.LoadScene( "L_LoadoutMenu" );
+	}
+
+    /// <summary>
+	/// Close the game
+	/// </summary>
+    public void Exit()
+	{
+        Application.Quit();
+	}
+
+    public void ReturnToMain()
+	{
+        SceneManager.LoadScene( "L_MainMenu" );
+	}
+}
