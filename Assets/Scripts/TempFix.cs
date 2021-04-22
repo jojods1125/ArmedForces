1e7e4a1762477c6ccf09e3cf1ca8d37c3606a3e2
Logan Hobbs <loganhbz@live.com>
2021-04-22 17:01:31 -0400

    Fixed Selection Escaping
    Players now unable to escape the selection screen when multiple players exists

--- Assets/Scripts/TempFix.cs
+++ Assets/Scripts/TempFix.cs
@@ -11,12 +11,27 @@ public class TempFix : MonoBehaviour
     public MultiplayerEventSystem mes;
     public PlayerInput pi;
 
+    public int playerId;
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Gamepad.current[GamepadButton.RightTrigger].isPressed && Gamepad.current[GamepadButton.LeftTrigger].isPressed)
+        // currently selected object
+        GameObject curr = mes.currentSelectedGameObject;
+
+        // is Selection active
+        if (transform.Find("Selection").gameObject.activeSelf)
+        {
+            // scrollbar is not selected, curr->Weapons->Weapon Group Type->Content->Viewport->ScrollView->Player Input #
+            if (curr != scroller && !curr.transform.parent.parent.parent.parent.parent.parent.parent.name.Contains(playerId.ToString()))
+            {
+                mes.SetSelectedGameObject(scroller);
+            }
+        }
+/*        // if Selection not active, curr->(Back/Front) Arm Loadout->Loadouts->Player Input #
+        else if (!curr.transform.parent.parent.parent.name.Contains(playerId.ToString()))
         {
             mes.SetSelectedGameObject(scroller);
-        }
+        }*/
     }
 }
