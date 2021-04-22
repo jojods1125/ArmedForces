302b70f5eb4fdfd535f1cf185ef2a3fdb2fb8ba6
Logan Hobbs <loganhbz@live.com>
2021-04-22 14:22:31 -0400

    Pushing current Work
    Going to review PR, have to push

---
+++ Assets/Scripts/TempFix.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.UI;
+
+public class TempFix : MonoBehaviour
+{
+    public GameObject scroller;
+    public MultiplayerEventSystem mes;
+    public PlayerInput pi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Gamepad.current[GamepadButton.RightTrigger].isPressed && Gamepad.current[GamepadButton.LeftTrigger].isPressed)
+        {
+            mes.SetSelectedGameObject(scroller);
+        }
+    }
+}
