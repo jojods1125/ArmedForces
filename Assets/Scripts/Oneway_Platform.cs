1d7d518ff7d1578c5eac7be7da7a2e0342b90031
Isaac Sheridan <isaacjohnsheridan@gmail.com>
2021-04-22 22:16:53 -0400

    One way collision object made, needs tweaking, only works on left localMulti Platform

---
+++ Assets/Scripts/Oneway_Platform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oneway_Platform : MonoBehaviour
+{
+    public GameObject platform;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("in");
+        Physics.IgnoreCollision(other.GetComponent<Collider>(), platform.GetComponent<Collider>(), true);
+    }
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("out");
+        Physics.IgnoreCollision(other.GetComponent<Collider>(), platform.GetComponent<Collider>(), false);
+    }
+}
